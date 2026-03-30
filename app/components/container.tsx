2ad8305a3441d4f04d1be2fab9f39d941871ecaa
Erick <mosericko@gmail.com>
2026-03-30 08:24:54 +0300

    feat: add container component

---
+++ app/components/container.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import { type ReactNode } from "react";
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+
+const Container = ({ children, className, ...props }: Props) => {
+  return (
+    <div
+      className={cn(className, "container mx-auto p-5 2xl:max-w-7xl 2xl:p-0")}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
